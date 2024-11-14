b86edf4ccd0899e613bcf120460031c7bfc87e37
Marcin Stożek <100865662+Keshy12@users.noreply.github.com>
2024-11-14 10:36:24 +0100

    Excercise 2
     Recursive Directory Size Calculation

---
+++ excercise2.php
@@ -0,0 +1,29 @@
+<?php
+$data = [
+    "file1.txt" => 200,
+    "file2.txt" => 300,
+    "subdir1" => [
+        "file3.txt" => 150,
+        "file4.txt" => 250,
+        "subdir2" => [
+            "file5.txt" => 100
+        ]
+    ]
+];
+
+/**
+* Might hit recursion depth limit.
+* Depending on the input data, iterative approach might be better.
+*/
+function calculateDirectorySize($data): int {
+    $size = array_reduce($data, function($carry, $item) {
+        if (is_array($item)) {
+            return $carry + calculateDirectorySize($item);
+        }
+        return $carry + $item;
+    }, 0);
+    
+    return $size;
+}
+
+print_r(calculateDirectorySize($data));
